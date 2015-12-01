23534dae9eafacb8cbc6ae165a28571b7910b343
Jared Anderson <jared@tuxsudo.com>
2015-12-01 10:31:27 -0500

    solution for Lab2

--- Lab2/app.js
+++ Lab2/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
 
 class HelloWorld extends React.Component {
   render() {
-    var now = new Date();
+    var now = moment(new Date())
+      .format('MMMM Do YYYY, h:mm:ss a');
+      
     return (
       <div><h1>Hello World!</h1>
       <h2>The current time is: {now.toString()}</h2>
