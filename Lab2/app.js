55b25e39708f61fdd912447cc7d3079ba245ca12
Jared Anderson <jared@tuxsudo.com>
2015-12-01 10:33:45 -0500

    Fixed Lab2 solution

--- Lab2/app.js
+++ Lab2/app.js
@@ -6,11 +6,12 @@ class HelloWorld extends React.Component {
   render() {
     var now = moment(new Date())
       .format('MMMM Do YYYY, h:mm:ss a');
-      
+
     return (
-      <div><h1>Hello World!</h1>
-      <h2>The current time is: {now.toString()}</h2>
-      </div>
+        <div>
+            <h1>Hello World!</h1>
+            <h2>The current time is: {now}</h2>
+        </div>
     );
   }
 }
