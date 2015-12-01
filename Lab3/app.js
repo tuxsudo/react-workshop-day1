5d7cdebed429dc8e552ecfc86f8fb05728a66a3d
Jared Anderson <jared@tuxsudo.com>
2015-12-01 10:45:57 -0500

    Solution for Lab3

--- Lab3/app.js
+++ Lab3/app.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import moment from 'moment'
+import moment from 'moment';
+import {Button, Jumbotron} from 'react-bootstrap';
 
 class HelloWorld extends React.Component {
   render() {
     var now = new Date();
     var formattedDate = moment(now).format('MMMM Do YYYY, h:mm:ss A');
     return (
-      <div><h1>Welcome to Fort Lauderdale!</h1>
-      <h2>The current time is: {formattedDate}</h2>
-      </div>
+        <Jumbotron>
+            <h1>Welcome to Fort Lauderdale!</h1>
+            <p>The current time is: {formattedDate}</p>
+            <Button>Learn More</Button>
+        </Jumbotron>
     );
   }
 }
