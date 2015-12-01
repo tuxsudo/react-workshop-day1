d3a5dd265cda6d390fd09e8dfd5f929df8dfebaa
Jared Anderson <jared@tuxsudo.com>
2015-12-01 14:43:05 -0500

    Finish Lab 8

--- Lab8/app.js
+++ Lab8/app.js
@@ -8,68 +8,106 @@ import Input from 'react-bootstrap/lib/Input';
 import Label from 'react-bootstrap/lib/Label';
 
 class LunchApp extends React.Component {
-  render() {
-    var now = new Date();
-    var formattedDate = moment(now).format('MMMM Do YYYY');
-    return (
-      <div>
-        <Panel>
-          <h2>Options for lunch for {formattedDate}:</h2>
-          <LunchOptionsPanel lunchData={this.props.lunchChoices}> </LunchOptionsPanel>
-        </Panel>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: 'Nothing :(',
+            instructions: 'none'
+        };
+    }
+
+    updateSelection(selected) {
+        // may need to use innerText for older IE
+        this.setState({selected});
+    }
+
+    updateInstructions(instructions) {
+        this.setState({ instructions });
+    }
+
+
+    render() {
+        var now = new Date();
+        var formattedDate = moment(now).format('MMMM Do YYYY');
+        return (
+            <div>
+                <Panel>
+                    <h2>Options for lunch for {formattedDate}:</h2>
+                    <LunchOptionsPanel
+                        lunchData={this.props.lunchChoices}
+                        selected={this.state.selected}
+                        instructions={this.state.instructions}
+                        updateSelection={this.updateSelection.bind(this)}
+                        updateInstructions={this.updateInstructions.bind(this)}
+                    />
+                </Panel>
+            </div>
+        );
+    }
 }
 
+
+class SpecialInstructionsInput extends React.Component {
+
+    render() {
+        return (
+            <Panel header="Please enter special instructions">
+                <Input type="text" value={this.props.value} onChange={e=>this.props.changeHandler(e.target.value) } />
+            </Panel>
+        );
+    }
+
+}
+
+
 class LunchOptionsPanel extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {selectedLunch: 'Nothing selected'};
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick(event) {
-    // may need to use innerText for older IE
-    this.setState({
-      selectedLunch: event.target.textContent
-    });
-  }
-  render() {
-    let clickHandler = this.handleClick;
-    let lunchOptions = this.props.lunchData.map(function(c,i) {
-      return <h3 key={i} onClick={clickHandler}><Label>{c}</Label></h3>;
-    });
-    return (
-      <div>
-        <Panel header="Please select one" bsStyle="info">
-          {lunchOptions}
-        </Panel>
-        <SelectedLunchPanel  selectedLunch={this.state.selectedLunch}></SelectedLunchPanel>
-      </div>
-    );
-  }
+
+    render() {
+
+        let lunchOptions = this.props.lunchData.map( (c,i)=> (
+            <h3 key={i} onClick={ e=>this.props.updateSelection(e.target.textContent) }>
+                <Label bsStyle={c===this.props.selected?"primary":"default"}>{c}</Label>
+            </h3>
+        ));
+
+        return (
+            <div>
+                <Panel header="Please select one" bsStyle="info">
+                    {lunchOptions}
+                </Panel>
+
+                <SpecialInstructionsInput
+                    changeHandler={ this.props.updateInstructions }
+                />
+
+                <SelectedLunchPanel
+                    selectedLunch={this.props.selected}
+                    instructions={this.props.instructions}
+                />
+
+            </div>
+        );
+    }
 }
 
 class SelectedLunchPanel extends React.Component {
-  constructor(props) {
-    super(props);
-    this.updateInstructions = this.updateInstructions.bind(this);
-    this.state = { instructions: '' };
-  }
-  render() {
-    return (
-      <div>
-        <Panel header="You've picked" bsStyle="warning">
-          <Label>{this.props.selectedLunch}</Label>
-          <p>Special Instructions: </p>
-        </Panel>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div>
+                <Panel header="You've picked" bsStyle="warning">
+                    <h1><Label>{this.props.selectedLunch}</Label></h1>
+                    <hr />
+                    <h3>Special Instructions: <Label>{this.props.instructions}</Label></h3>
+                </Panel>
+            </div>
+        );
+    }
 }
 
 var lunchChoices = ['Chicken', 'Fish', 'Vegetarian'];
 ReactDOM.render(
-  <LunchApp lunchChoices={lunchChoices}/>,
-  document.getElementById('root')
+    <LunchApp lunchChoices={lunchChoices}/>,
+    document.getElementById('root')
 );
